d1db00123ee568ada859704d4c499ebb99611039
Andrew <324.andrew.wang@gmail.com>
2020-04-21 19:53:32 -0400

    added the matching brackets solution

---
+++ files/matchingBrackets.py
@@ -0,0 +1,33 @@
+import collections
+# s = {{{}}}
+    
+def balancedString(s):
+    stack = collections.deque()
+    bracketDict = collections.defaultdict()
+    bracketDict['}'] = '{'
+    bracketDict[']'] = '['
+    bracketDict[')'] = '('
+    prevTop = ''
+
+    for char in s: # {{{}}     stack: {     prevTop: {
+        if stack:
+            prevTop = stack[-1]
+
+        if char == '(' or char == '{' or char == '[':
+            stack.append(char)
+        elif bracketDict[char] == prevTop:
+            stack.pop()
+        else:
+            return False
+
+    if not stack:
+        return True
+    else:
+        return False
+        
+print(balancedString("{{{}}}")) #True
+print(balancedString("")) #True 
+print(balancedString("{[}]")) #False
+print(balancedString("}}}")) #False
+print(balancedString("{[()(")) #False
+print(balancedString("{}[")) #False
